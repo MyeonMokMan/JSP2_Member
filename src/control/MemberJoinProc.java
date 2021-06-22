76db06949715171dc1017fd31b831830805854a8
MyeonMokMan <oniic@naver.com>
2021-06-22 15:48:47 +0900

    JAVA파일
    회원 가입시에 입력한 폼을 빈클래스에 저장하는 메소드 (비밀번호 같을시에 저장 완료, 틀릴시에 에러 메세지)

--- src/control/MemberJoinProc.java
+++ src/control/MemberJoinProc.java
@@ -1,6 +1,8 @@
 package control;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -8,6 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import model.MemberBean;
+import model.MemberDAO;
 
 /**
  * Servlet implementation class MemberJoinProc
@@ -25,11 +28,15 @@ public class MemberJoinProc extends HttpServlet {
 	
 	protected void reqPro(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
+		request.setCharacterEncoding("euc-kr");
+		
 		MemberBean bean = new MemberBean();
 		
 		bean.setId(request.getParameter("id"));
-		bean.setPass1(request.getParameter("pass1"));
-		bean.setPass2(request.getParameter("pass2"));
+		String pass1 = request.getParameter("pass1");
+		String pass2 = request.getParameter("pass2");
+		bean.setPass1(pass1);
+		bean.setPass2(pass2);
 		bean.setEmail(request.getParameter("email"));
 		bean.setTel(request.getParameter("tel"));
 		String [] hobbyArr = request.getParameterValues("hobby");
@@ -37,9 +44,26 @@ public class MemberJoinProc extends HttpServlet {
 		for(String data1 : hobbyArr) {
 			data += data1+" ";
 		}
-		bean.setHobby("data");
+		bean.setHobby(data);
 		bean.setJob(request.getParameter("job"));
 		bean.setAge(request.getParameter("age"));
 		bean.setInfo(request.getParameter("info"));
-	}
+		
+		//패스워드가 같을 경우에만 데이터베이스에 저장
+		if(pass1.equals(pass2)) {
+			
+			//데이터베이스 객체 생성
+			MemberDAO mdao = new MemberDAO();
+			mdao.insertMember(bean);
+			
+			RequestDispatcher dis = request.getRequestDispatcher("MemberList.jsp");
+			dis.forward(request, response);
+			
+		} else {
+			
+			request.setAttribute("msg", "패스워드가 일치하지 않습니다.");
+			RequestDispatcher dis = request.getRequestDispatcher("LoginError.jsp");
+			dis.forward(request, response);
+		}
+	} 
 }
